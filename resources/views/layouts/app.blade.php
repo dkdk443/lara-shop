2c896c2c04df5553d740472d9c57c41c823fb815
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2021-02-23 21:40:52 +0900

    ビューのテンプレート化

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,72 +29,76 @@
 
 <body class="">
   <div class="wrapper ">
-    <div class="sidebar" data-color="orange">
-      <div class="logo">
-        <a href="" class="simple-text logo-mini">
-          TOP
-        </a>
-        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
-          Creative Tim
-        </a>
+    @section('sidebar')
+
+<div class="sidebar" data-color="orange">
+    <div class="logo">
+          <a href="" class="simple-text logo-mini">
+            TOP
+          </a>
+          <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+            Creative Tim
+          </a>
+        </div>
+        <div class="sidebar-wrapper" id="sidebar-wrapper">
+          <ul class="nav">
+            <li class="active ">
+              <a href="/">
+                <i class="now-ui-icons design_app"></i>
+                <p>Dashboard</p>
+              </a>
+            </li>
+            <li>
+              <a href="./staffs">
+                <i class="now-ui-icons education_atom"></i>
+                <p>スタッフ</p>
+              </a>
+            </li>
+            <li>
+              <a href="./map.html">
+                <i class="now-ui-icons location_map-big"></i>
+                <p>Maps</p>
+              </a>
+            </li>
+            <li>
+              <a href="./notifications.html">
+                <i class="now-ui-icons ui-1_bell-53"></i>
+                <p>Notifications</p>
+              </a>
+            </li>
+            <li>
+              <a href="./user.html">
+                <i class="now-ui-icons users_single-02"></i>
+                <p>User Profile</p>
+              </a>
+            </li>
+            <li>
+              <a href="./tables.html">
+                <i class="now-ui-icons design_bullet-list-67"></i>
+                <p>Table List</p>
+              </a>
+            </li>
+            <li>
+              <a href="./typography.html">
+                <i class="now-ui-icons text_caps-small"></i>
+                <p>Typography</p>
+              </a>
+            </li>
+            <li class="active-pro">
+              <a href="./upgrade.html">
+                <i class="now-ui-icons arrows-1_cloud-download-93"></i>
+                <p>Upgrade to PRO</p>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="sidebar-wrapper" id="sidebar-wrapper">
-        <ul class="nav">
-          <li class="active ">
-            <a href="/">
-              <i class="now-ui-icons design_app"></i>
-              <p>Dashboard</p>
-            </a>
-          </li>
-          <li>
-            <a href="./staffs">
-              <i class="now-ui-icons education_atom"></i>
-              <p>スタッフ</p>
-            </a>
-          </li>
-          <li>
-            <a href="./map.html">
-              <i class="now-ui-icons location_map-big"></i>
-              <p>Maps</p>
-            </a>
-          </li>
-          <li>
-            <a href="./notifications.html">
-              <i class="now-ui-icons ui-1_bell-53"></i>
-              <p>Notifications</p>
-            </a>
-          </li>
-          <li>
-            <a href="./user.html">
-              <i class="now-ui-icons users_single-02"></i>
-              <p>User Profile</p>
-            </a>
-          </li>
-          <li>
-            <a href="./tables.html">
-              <i class="now-ui-icons design_bullet-list-67"></i>
-              <p>Table List</p>
-            </a>
-          </li>
-          <li>
-            <a href="./typography.html">
-              <i class="now-ui-icons text_caps-small"></i>
-              <p>Typography</p>
-            </a>
-          </li>
-          <li class="active-pro">
-            <a href="./upgrade.html">
-              <i class="now-ui-icons arrows-1_cloud-download-93"></i>
-              <p>Upgrade to PRO</p>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    @show
 
     <div class="main-panel" id="main-panel">
       <!-- Navbar -->
-      <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
+      @section('navbar')
+        <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
         <div class="container-fluid">
           <div class="navbar-wrapper">
             <div class="navbar-toggle">
@@ -156,10 +160,14 @@
           </div>
         </div>
       </nav>
+      @show
+
       <!-- End Navbar -->
-      <div class="panel-header panel-header-lg">
+      @section('panel-header')
+        <div class="panel-header panel-header-lg">
         <canvas id="bigDashboardChart"></canvas>
       </div>
+      @show
       <div class="content">
           @yield('content')
       </div>
--- resources/views/staffs/create.blade.php
+++ resources/views/staffs/create.blade.php
@@ -1,275 +1,57 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-  <!--     Fonts and icons     -->
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <!-- CSS Files -->
-
-<!-- CSRF Token -->
-<meta name="csrf-token" content="{{ csrf_token() }}">
-
-<title>{{ config('app.name', 'Laravel') }}</title>
-
-<script src="{{ mix('js/app.js') }}" defer></script>
-
-<!-- Fonts -->
-<link rel="dns-prefetch" href="//fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-<!-- Styles -->
-<link href="{{ mix('css/app.css') }}" rel="stylesheet">
-</head>
-
-<body class="">
-  <div class="wrapper ">
-    <div class="sidebar" data-color="orange">
-      <div class="logo">
-        <a href="" class="simple-text logo-mini">
-          TOP
-        </a>
-        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
-          Creative Tim
-        </a>
-      </div>
-      <div class="sidebar-wrapper" id="sidebar-wrapper">
-        <ul class="nav">
-          <li class="active ">
-            <a href="/">
-              <i class="now-ui-icons design_app"></i>
-              <p>Dashboard</p>
-            </a>
-          </li>
-          <li>
-            <a href="./staffs">
-              <i class="now-ui-icons education_atom"></i>
-              <p>スタッフ</p>
-            </a>
-          </li>
-          <li>
-            <a href="./map.html">
-              <i class="now-ui-icons location_map-big"></i>
-              <p>Maps</p>
-            </a>
-          </li>
-          <li>
-            <a href="./notifications.html">
-              <i class="now-ui-icons ui-1_bell-53"></i>
-              <p>Notifications</p>
-            </a>
-          </li>
-          <li>
-            <a href="./user.html">
-              <i class="now-ui-icons users_single-02"></i>
-              <p>User Profile</p>
-            </a>
-          </li>
-          <li>
-            <a href="./tables.html">
-              <i class="now-ui-icons design_bullet-list-67"></i>
-              <p>Table List</p>
-            </a>
-          </li>
-          <li>
-            <a href="./typography.html">
-              <i class="now-ui-icons text_caps-small"></i>
-              <p>Typography</p>
-            </a>
-          </li>
-          <li class="active-pro">
-            <a href="./upgrade.html">
-              <i class="now-ui-icons arrows-1_cloud-download-93"></i>
-              <p>Upgrade to PRO</p>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-
-    <div class="main-panel" id="main-panel">
-      <!-- Navbar -->
-      <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
-        <div class="container-fluid">
-          <div class="navbar-wrapper">
-            <div class="navbar-toggle">
-              <button type="button" class="navbar-toggler">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-              </button>
+@extends('layouts.app')
+@section('content')
+<div class="content">
+    <div class="row">
+        <div class="col-md-8">
+          <div class="card">
+            <div class="card-header">
+              <h5 class="title">スタッフ登録画面</h5>
             </div>
-            <a class="navbar-brand" href="#pablo">Dashboard</a>
-          </div>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navigation">
-            <form>
-              <div class="input-group no-border">
-                <input type="text" value="" class="form-control" placeholder="Search...">
-                <div class="input-group-append">
-                  <div class="input-group-text">
-                    <i class="now-ui-icons ui-1_zoom-bold"></i>
+            <div class="card-body">
+              <form method="POST" action="/staffs">
+                @csrf
+                <div class="row">
+                  <div class="col-md-5 pr-1">
+                    <div class="form-group">
+                      <label>名前</label>
+                      <input type="text" class="form-control" placeholder="田中 太郎" value="" name="staff_name">
+                    </div>
                   </div>
                 </div>
-              </div>
-            </form>
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="#pablo">
-                  <i class="now-ui-icons media-2_sound-wave"></i>
-                  <p>
-                    <span class="d-lg-none d-md-block">Stats</span>
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="now-ui-icons location_world"></i>
-                  <p>
-                    <span class="d-lg-none d-md-block">Some Actions</span>
-                  </p>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                  <a class="dropdown-item" href="#">Action</a>
-                  <a class="dropdown-item" href="#">Another action</a>
-                  <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#pablo">
-                  <i class="now-ui-icons users_single-02"></i>
-                  <p>
-                    <span class="d-lg-none d-md-block">Account</span>
-                  </p>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <!-- End Navbar -->
-      <div class="panel-header panel-header-lg">
-        <canvas id="bigDashboardChart"></canvas>
-      </div>
-      <div class="content">
-        <div class="row">
-            <div class="col-md-8">
-              <div class="card">
-                <div class="card-header">
-                  <h5 class="title">スタッフ登録画面</h5>
-                </div>
-                <div class="card-body">
-                  <form method="POST" action="/staffs">
-                    @csrf
-                    <div class="row">
-                      <div class="col-md-5 pr-1">
-                        <div class="form-group">
-                          <label>名前</label>
-                          <input type="text" class="form-control" placeholder="田中 太郎" value="" name="staff_name">
-                        </div>
-                      </div>
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="form-group">
+                        <label>メールアドレス</label>
+                        <input type="email" class="form-control" placeholder="" value="" name="email">
                     </div>
-                    <div class="row">
-                      <div class="col-md-12">
-                        <div class="form-group">
-                            <label>メールアドレス</label>
-                            <input type="email" class="form-control" placeholder="" value="" name="email">
-                        </div>
-                        <div class="form-group">
-                            <label>パスワード</label>
-                            <input type="password" class="form-control" placeholder="" value="" name="password">
-                        </div>
-                      </div>
+                    <div class="form-group">
+                        <label>パスワード</label>
+                        <input type="password" class="form-control" placeholder="" value="" name="password">
                     </div>
+                  </div>
+                </div>
 
-                    <div>
-                    @if (count($errors) > 0)
-                    <div class="alert alert-danger">
-                        {{-- <button type="button" aria-hidden="true" class="close">
-                          <i class="now-ui-icons ui-1_simple-remove"></i>
-                        </button> --}}
-                        @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </div>
-                    @endif
+                <div>
+                @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    {{-- <button type="button" aria-hidden="true" class="close">
+                      <i class="now-ui-icons ui-1_simple-remove"></i>
+                    </button> --}}
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </div>
+                @endif
 
-                    </div>
-                    <div>
-                        <button type="button" class="btn btn-secondary">キャンセル</button>
-                        <button class="btn btn-outline-primary" type="submit">保存</button>
-                    </div>
-                  </form>
                 </div>
-              </div>
+                <div>
+                    <a type="button" class="btn btn-secondary" href="{{route('staffs.index')}}">キャンセル</a>
+                    <button class="btn btn-outline-primary" type="submit">保存</button>
+                </div>
+              </form>
             </div>
           </div>
-      </div>
-      <footer class="footer">
-        <div class=" container-fluid ">
-          <nav>
-            <ul>
-              <li>
-                <a href="https://www.creative-tim.com">
-                  Creative Tim
-                </a>
-              </li>
-              <li>
-                <a href="http://presentation.creative-tim.com">
-                  About Us
-                </a>
-              </li>
-              <li>
-                <a href="http://blog.creative-tim.com">
-                  Blog
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <div class="copyright" id="copyright">
-            &copy; <script>
-              document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
-            </script>, Designed by <a href="https://www.invisionapp.com" target="_blank">Invision</a>. Coded by <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
-          </div>
         </div>
-      </footer>
-    </div>
+      </div>
   </div>
-  <!--   Core JS Files   -->
-  <script src="../assets/js/core/jquery.min.js"></script>
-  <script src="../assets/js/core/popper.min.js"></script>
-  <script src="../assets/js/core/bootstrap.min.js"></script>
-  <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-  <!--  Google Maps Plugin    -->
-  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-  <!-- Chart JS -->
-  <script src="../assets/js/plugins/chartjs.min.js"></script>
-  <!--  Notifications Plugin    -->
-  <script src="../assets/js/plugins/bootstrap-notify.js"></script>
-  <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="../assets/js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script><!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
-  <script src="../assets/demo/demo.js"></script>
-  <script>
-    $(document).ready(function() {
-      // Javascript method's body can be found in assets/js/demos.js
-      demo.initDashboardPageCharts();
-
-    });
-  </script>
-</body>
-
-</html>
-
-
-
-
-
+@endsection
